d80adcb5e6c1e31e1828f827b55604bcbaad6b56
Michał Domagalski <michal.domagalski.krakow@gmail.com>
2022-03-12 17:34:49 +0100

    feat: transaction management

--- src/places/places.service.ts
+++ src/places/places.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 
 import { Place } from './models/place.model';
+import { Transaction } from 'sequelize';
 
 @Injectable()
 export class PlacesService {
@@ -10,9 +11,9 @@ export class PlacesService {
     private readonly placeModel: typeof Place,
   ) {}
 
-  public async getAllPlaces(): Promise<Place[]> {
+  public async getAllPlaces(transaction: Transaction): Promise<Place[]> {
     try {
-      return await this.placeModel.findAll();
+      return await this.placeModel.findAll({ transaction });
     } catch (error) {
       // TODO: return handled error gracefully
       throw error;
