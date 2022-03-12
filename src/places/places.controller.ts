28f7ad07eb7056e37d7f33a1e42dddbe91274f33
Michał Domagalski <michal.domagalski.krakow@gmail.com>
2022-03-12 16:30:35 +0100

    chore: add prettier commands and run prettier against current files

--- src/places/places.controller.ts
+++ src/places/places.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Post} from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 import { PlacesService } from './places.service';
 import { Place } from './models/place.model';
