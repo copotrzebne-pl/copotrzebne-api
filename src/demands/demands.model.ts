a3f9347956018b4af4d053265c3217010cbf8d7e
Michał Domagalski <michal.domagalski.krakow@gmail.com>
2022-03-13 16:51:07 +0100

    fix: fix associations

--- src/demands/demands.model.ts
+++ src/demands/demands.model.ts
@@ -1,15 +1,27 @@
-import { BelongsTo, Column, DataType, Model, Table } from 'sequelize-typescript';
+import { BelongsTo, Column, DataType, ForeignKey, Model, Table } from 'sequelize-typescript';
 import { Supply } from '../supplies/supplies.model';
 import { Priority } from '../priorities/priorities.model';
 import { Place } from '../places/places.model';
 
-@Table({ tableName: 'supplies', underscored: true })
+@Table({ tableName: 'demands', underscored: true })
 export class Demand extends Model {
   @Column({ type: DataType.UUID, primaryKey: true, allowNull: false })
   id!: string;
 
   @Column({ allowNull: true, type: DataType.STRING })
   comment!: string | null;
+
+  @Column({ allowNull: false, type: DataType.UUID })
+  @ForeignKey(() => Place)
+  placeId!: string;
+
+  @Column({ allowNull: false, type: DataType.UUID })
+  @ForeignKey(() => Supply)
+  supplyId!: string | null;
+
+  @Column({ allowNull: true, type: DataType.UUID })
+  @ForeignKey(() => Priority)
+  priorityId!: string | null;
 
   @BelongsTo(() => Place)
   place!: Place | null;
