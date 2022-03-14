bffc414b5ed986358c872ce8de43d841619dba21
Adrian Kakol <adrian.kakol1@gmail.com>
2022-03-14 21:45:26 +0100

    Cors (#18)
    * allow cors
    
    * allow cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService: ConfigService = app.get(ConfigService);
   app.enableShutdownHooks();
   app.useGlobalPipes(new ValidationPipe());
