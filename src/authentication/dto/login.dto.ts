e0bfa2701526e7c13bb908c92397c47f905a57f1
adrkakol <adrian.kakol1@gmail.com>
2022-03-14 18:55:40 +0100

    fix: swagger url

--- src/authentication/dto/login.dto.ts
+++ src/authentication/dto/login.dto.ts
@@ -1,10 +1,13 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   login!: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   password!: string;
--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     .setDescription('Description of copotrzebne.pl API')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(configService.get<number>('PORT', 3000));
 }
