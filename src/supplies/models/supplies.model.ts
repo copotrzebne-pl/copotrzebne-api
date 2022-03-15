2261caaf14c5f1153c53c8b336aff31a715841ac
Adrian Kakol <adrian.kakol1@gmail.com>
2022-03-15 17:32:08 +0100

    fix: cleanup migrations, fix default id values, fix models (#19)
    * chore: cleanup migrations
    
    * fix: remove old migrations, create one migration with fixes, update models
    
    * fix test

--- src/supplies/models/supplies.model.ts
+++ src/supplies/models/supplies.model.ts
@@ -1,16 +1,9 @@
-import { Column, DataType, HasMany, Model, Table } from 'sequelize-typescript';
+import { Column, DataType, HasMany, Model, Sequelize, Table } from 'sequelize-typescript';
 import { Demand } from '../../demands/models/demands.model';
 
-@Table({ tableName: 'supplies', underscored: true })
+@Table({ tableName: 'supplies', underscored: true, timestamps: false })
 export class Supply extends Model {
-  @Column({
-    type: DataType.UUID,
-    primaryKey: true,
-    allowNull: false,
-    unique: true,
-    autoIncrement: false,
-    defaultValue: DataType.UUIDV4,
-  })
+  @Column({ primaryKey: true, defaultValue: Sequelize.fn('uuid_generate_v4') })
   id!: string;
 
   @Column({ allowNull: false, type: DataType.STRING })
--- src/users/models/user.model.ts
+++ src/users/models/user.model.ts
@@ -1,17 +1,10 @@
-import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import { Column, DataType, Model, Sequelize, Table } from 'sequelize-typescript';
 
 import { UserRole } from '../types/user-role.enum';
 
 @Table({ tableName: 'users', underscored: true })
 export class User extends Model {
-  @Column({
-    allowNull: false,
-    type: DataType.UUID,
-    primaryKey: true,
-    unique: true,
-    autoIncrement: false,
-    defaultValue: DataType.UUIDV4,
-  })
+  @Column({ primaryKey: true, defaultValue: Sequelize.fn('uuid_generate_v4') })
   id!: string;
 
   @Column({ allowNull: false, type: DataType.STRING })
