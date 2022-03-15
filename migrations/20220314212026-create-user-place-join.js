fcffd670b5da7e981b16b43ebb9360f50b861ae8
adrkakol <adrian.kakol1@gmail.com>
2022-03-15 18:00:27 +0100

    add unique constraint

--- migrations/20220314212026-create-user-place-join.js
+++ migrations/20220314212026-create-user-place-join.js
@@ -2,28 +2,38 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('user_place', {
-      user_id: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        references: {
-          model: 'users',
-          key: 'id',
+    await queryInterface.createTable(
+      'user_place',
+      {
+        user_id: {
+          type: Sequelize.UUID,
+          allowNull: false,
+          references: {
+            model: 'users',
+            key: 'id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
         },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
-      },
-      place_id: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        references: {
-          model: 'places',
-          key: 'id',
+        place_id: {
+          type: Sequelize.UUID,
+          allowNull: false,
+          references: {
+            model: 'places',
+            key: 'id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
         },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
       },
-    });
+      {
+        uniqueKeys: {
+          user_place_unique_index: {
+            fields: ['user_id', 'place_id'],
+          },
+        },
+      },
+    );
   },
 
   async down(queryInterface, Sequelize) {
