3765299f84a5c6c7f20dcb04410bfe84e53c2da2
Michał Domagalski <michal.domagalski.krakow@gmail.com>
2022-03-23 12:14:18 +0100

    feat: fix migration

--- migrations/20220323083321-add-categories-priorities.js
+++ migrations/20220323083321-add-categories-priorities.js
@@ -12,6 +12,105 @@ module.exports = {
         },
         { transaction },
       );
+
+      await queryInterface.bulkUpdate(
+        'categories',
+        {
+          priority: 1,
+        },
+        {
+          name_pl: 'żywność trwała',
+        },
+        { transaction },
+      );
+
+      await queryInterface.bulkUpdate(
+        'categories',
+        {
+          priority: 2,
+        },
+        {
+          name_pl: 'żywność krótkotrwała',
+        },
+        { transaction },
+      );
+
+      await queryInterface.bulkUpdate(
+        'categories',
+        {
+          priority: 3,
+        },
+        {
+          name_pl: 'środki higieny osobistej i chemia',
+        },
+        { transaction },
+      );
+
+      await queryInterface.bulkUpdate(
+        'categories',
+        {
+          priority: 4,
+        },
+        {
+          name_pl: 'dzieci',
+        },
+        { transaction },
+      );
+
+      await queryInterface.bulkUpdate(
+        'categories',
+        {
+          priority: 5,
+        },
+        {
+          name_pl: 'leki',
+        },
+        { transaction },
+      );
+
+      await queryInterface.bulkUpdate(
+        'categories',
+        {
+          priority: 6,
+        },
+        {
+          name_pl: 'zwierzęta',
+        },
+        { transaction },
+      );
+
+      await queryInterface.bulkUpdate(
+        'categories',
+        {
+          priority: 7,
+        },
+        {
+          name_pl: 'odzież i tekstylia',
+        },
+        { transaction },
+      );
+
+      await queryInterface.bulkUpdate(
+        'categories',
+        {
+          priority: 8,
+        },
+        {
+          name_pl: 'inne',
+        },
+        { transaction },
+      );
+
+      await queryInterface.bulkUpdate(
+        'categories',
+        {
+          priority: 9,
+        },
+        {
+          name_pl: 'zupa dla Ukrainy',
+        },
+        { transaction },
+      );
     });
   },
 
