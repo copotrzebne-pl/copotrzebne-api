bef6a87cc69c91af584f3cbc80e8173d97065880
Michał Domagalski <michal.domagalski.krakow@gmail.com>
2022-04-08 20:09:33 +0200

    feat: cleanup type annotations

--- src/places/services/state-machine/place.transitions.ts
+++ src/places/services/state-machine/place.transitions.ts
@@ -1,6 +1,7 @@
 import { PlaceState } from '../../types/place.state.enum';
+import { Transition } from '../../../state-machine/types/transition';
 
-export const placeTransitions = [
+export const placeTransitions: Transition[] = [
   { startState: PlaceState.ACTIVE, endState: PlaceState.INACTIVE, name: 'DEACTIVATE' },
   { startState: PlaceState.INACTIVE, endState: PlaceState.ACTIVE, name: 'ACTIVATE' },
 ];
